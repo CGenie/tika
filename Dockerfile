6bd250d049c5e17fc46943b5a1bc42efd77300e5
Przemek Kaminski <cgenie@gmail.com>
2017-01-03 10:22:09 +0000

    Update to tika 1.14, use openjdk-alpine

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM dockerfile/java
+FROM openjdk:7-alpine
 MAINTAINER Todd Tyree <tatyree@gmail.com>
+
+RUN apk add --update --no-cache curl
 
 RUN mkdir /setup
 ADD install.sh /setup/install.sh
 RUN /setup/install.sh
-ENTRYPOINT java -jar /srv/tika-server-1.6.jar -host 0.0.0.0
+ENTRYPOINT java -jar /srv/tika-server-1.14.jar -host 0.0.0.0
 
 EXPOSE 9998
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "# Installing Tika"
-curl http://www.apache.org/dist/tika/tika-server-1.6.jar -s -o /srv/tika-server-1.6.jar
+#curl http://www.apache.org/dist/tika/tika-server-1.6.jar -s -o /srv/tika-server-1.6.jar
+curl http://www.apache.org/dist/tika/tika-server-1.14.jar -s -o /srv/tika-server-1.14.jar
 
-echo "# Cleaning up"
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /setup /build
+#echo "# Cleaning up"
+#apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /setup /build
