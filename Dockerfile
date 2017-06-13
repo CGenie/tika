ec6119729ffc9c99cf4017456dd803f23de5e237
Przemek Kaminski <cgenie@gmail.com>
2017-06-13 14:30:21 +0100

    Upgrade to tika 1.15

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ RUN apk add --update --no-cache curl
 RUN mkdir /setup
 ADD install.sh /setup/install.sh
 RUN /setup/install.sh
-ENTRYPOINT java -jar /srv/tika-server-1.14.jar -host 0.0.0.0
+ENTRYPOINT java -jar /srv/tika-server-1.15.jar -host 0.0.0.0
 
 EXPOSE 9998
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "# Installing Tika"
 #curl http://www.apache.org/dist/tika/tika-server-1.6.jar -s -o /srv/tika-server-1.6.jar
-curl http://www.apache.org/dist/tika/tika-server-1.14.jar -s -o /srv/tika-server-1.14.jar
+curl http://www.apache.org/dist/tika/tika-server-1.15.jar -s -o /srv/tika-server-1.15.jar
 
 #echo "# Cleaning up"
 #apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /setup /build
